41840a03d2ee58e5426a2387ad2681f03f15d20c
Bryan Head <bryan.head@gmail.com>
2016-04-18 10:03:48 -0500

    Remove unnecessary try-catch

--- src/main/Hello.scala
+++ src/main/Hello.scala
@@ -11,11 +11,7 @@ class HelloScalaExtension extends DefaultClassManager {
 class HelloString extends Reporter {
   override def getSyntax = reporterSyntax(right = List(StringType), ret = StringType)
   def report(args: Array[Argument], context: Context): AnyRef = {
-    val name = try args(0).getString
-    catch {
-      case e: LogoException =>
-        throw new ExtensionException(e.getMessage)
-    }
+    val name = args(0).getString
     "hello, " + name
   }
 }
